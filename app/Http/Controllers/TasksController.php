72320ef8a795d0d88ac6a87113a0b9b2468e78e9
Lucy Amelia Kirkby <lucy@lucyamellia.dev>
2022-10-04 21:01:17 +0100

    Authenticate as user 1

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -4,8 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Auth;
+
 class TasksController extends Controller
 {
+
+    /**
+     * Authenticates as first user as login system hasn't been implemented.
+     */
+    public function __construct(){
+        if(!Auth::user()) Auth::login(User::find(1));
+    }
+
     public function index(){
         return view('tasks');
     }
